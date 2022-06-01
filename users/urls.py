7d4cda6755811d6c3a71c9597b72bb24cf55da51
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-06-01 14:06:29 +0100

    pagde d'emploi de temps enseignant

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', accueil, name="accueil"),
+
+    path('enseignant/emploi_de_temps', enploiEnseignant, name='TimeTable')
 ]
--- users/views.py
+++ users/views.py
@@ -8,3 +8,10 @@ from django.shortcuts import render
 
 def accueil(request):
     return render(request, 'users/accueil.html')
+
+
+# ensegnant consulter son emploi de temps
+
+def enploiEnseignant(request):
+
+    return render(request, 'users/emploi_enseignant.html')
