f17d60eb5ccd7f219a0bb1c4df629fecb5ebfea4
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2022-06-01 09:05:42 +0100

    Création de la sideBar et initialisation de toutes les pages

--- emploi/urls.py
+++ emploi/urls.py
@@ -2,5 +2,24 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', dashboard, name="dashboard"),
+
+    # ======= PAGES VISIBLES AUX UTILISATEURS ====
+    path('', accueil, name="accueil"),
+    path('mon_planning', mon_planning, name="mon_planning"),
+    path('planning', planning, name="planning"),
+
+
+    # ======= PAGES DU DASBOARD =================
+    path('dasboard/', dashboard, name="dashboard"),
+    path('unite_enseignements/', unite_enseignements, name="unite_enseignements"),
+    path('unite_enseignements/', unite_enseignements, name="unite_enseignements"),
+    path('filieres/', filieres, name="filieres"),
+    path('specialite/', specialite, name="specialite"),
+    path('niveaux/', niveaux, name="niveaux"),
+    path('salles/', salles, name="salles"),
+    path('classes/', classes, name="classes"),
+    path('groupes/', groupes, name="groupes"),
+    path('unite_enseignements/', unite_enseignements, name="unite_enseignements"),
+    path('unite_enseignements/', unite_enseignements, name="unite_enseignements"),
+    path('unite_enseignements/', unite_enseignements, name="unite_enseignements"),
 ]
