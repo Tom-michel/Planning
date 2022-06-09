f01a096aa9bebb0c8b961f5b845a168da7f29142
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2022-06-09 10:31:22 +0100

    Ajout de la foctionnalité d'impréssion

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+    // immpression
+
+function imprimer(btn, element) {
+    btn.addEventListener("click", function() {
+        html2pdf(element);
+    });
+}
+    // btn_tel = document.getElementById('btn_tel');
+    // var element = document.getElementById('element-to-print');
+    // body = document.getElementsByTagName('body')
+    
+    
